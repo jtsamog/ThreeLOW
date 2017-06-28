fc8f9517b167a45516dfe1b37c9d78ab340f9221
Sam Ogunyowa <mailsamog@gmail.com>
2017-06-28 17:27:48 -0400

    Initial Commit

---
+++ ThreeLOW/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreeLOW
+//
+//  Created by Endeavour2 on 6/28/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
